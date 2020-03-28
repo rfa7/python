87683d1008c75eb97338d3964030d53b07f74980
Rafał Pietrykowski <rfa@gazeta.pl>
2020-03-28 23:08:48 +0100

    adding power of 2 placement

--- nauka/podaj.py
+++ nauka/podaj.py
@@ -1,18 +1,27 @@
-lista = [1,2,3,4,5,6,7,8]
+w = int(input('Podaj liczbę: '))
+lista = [x for x in range(1,w+1)]
+print('Lista start:', lista[0], 'Lista stop:', lista[-1])
 start = 1
 podano = [1]
-def podaj(start, lista):
+n = 0
+def podaj(start, lista, n):
     if len(lista) <= 1+start*2:
         print("KONIEC")
         # tu podano procedura
         lista = lista[start:]
-        podano.append(lista[:])
+        if len(lista) > 0:
+            podano.append(lista[:])
+            n += 1
         print(podano)
+        print(podano[-1])
+        print('start:', start,'|| N:', n, '|| N POWER 2 IS:',2**n, '|| Between...',
+              2**n, 'and ...', 2**(n+1))
         return podano
-    new = start*2
+    
     lista = lista[start:]
+    new = start*2
     podano.append(lista[0:new])
-    print('nowy start :', new, ' podano: ', podano)
-    podaj(new,lista)
+    # print('nowy start :', new, ' podano: ', podano, 'N: ', n)
+    podaj(new,lista, n+1)
 
-podaj(start,lista)
+podaj(start,lista, n)
